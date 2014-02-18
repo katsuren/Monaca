b78f4c824ec5ce585169b5c74f67d39df54e20d8
katsuren <t@k2rn.com>
2014-02-18 19:13:45 +0900

    Add controller and component

--- Controller/Component/MonadComponent.php
+++ Controller/Component/MonadComponent.php
@@ -3,6 +3,7 @@
 App::uses('Component', 'Controller');
 App::uses('CakeSession', 'Model/Datasource');
 App::uses('Maybe', 'Monaca.Lib');
+App::uses('Hash', 'Utility');
 
 class MonadComponent extends Component {
 
@@ -22,16 +23,16 @@ class MonadComponent extends Component {
 
 	public function getConfigOrElse($key, $default = null) {
 		if (Configure::check($key)) {
-			return new Maybe(Configure::read($key));
+			return Configure::read($key);
 		}
-		return new Maybe($default);
+		return $default;
 	}
 
 	public function getConfigOrCall($key, callable $function) {
 		if (Configure::check($key)) {
-			return new Maybe(Configure::read($key));
+			return Configure::read($key);
 		}
-		return new Maybe($function());
+		return $function();
 	}
 
 /**
@@ -39,7 +40,7 @@ class MonadComponent extends Component {
  */
 	public function getConfigOrThrow($key, \Exception $ex) {
 		if (Configure::check($key)) {
-			return new Maybe(Configure::read($key));
+			return Configure::read($key);
 		}
 		throw $ex;
 	}
@@ -54,16 +55,16 @@ class MonadComponent extends Component {
 
 	public function getSessionOrElse($key, $default = null) {
 		if (CakeSession::check($key)) {
-			return new Maybe(CakeSession::read($key));
+			return CakeSession::read($key);
 		}
-		return new Maybe($default);
+		return $default;
 	}
 
 	public function getSessionOrCall($key, callable $function) {
 		if (CakeSession::check($key)) {
-			return new Maybe(CakeSession::read($key));
+			return CakeSession::read($key);
 		}
-		return new Maybe($function());
+		return $function();
 	}
 
 /**
@@ -71,8 +72,115 @@ class MonadComponent extends Component {
  */
 	public function getSessionOrThrow($key, \Exception $ex) {
 		if (CakeSession::check($key)) {
-			return new Maybe(CakeSession::read($key));
+			return CakeSession::read($key);
 		}
 		throw $ex;
+	}
+
+	//-----------
+	// POST
+	//-----------
+
+	public function getPost($key) {
+		return $this->getPostOrElse($key, null);
+	}
+
+	public function getPostOrElse($key, $default = null) {
+		if ($key === null) {
+			return $_POST;
+		}
+		$ret = Hash::get($_POST, $key);
+		if ($ret !== null) {
+			return $ret;
+		}
+		return $default;
+	}
+
+	public function getPostOrCall($key, callable $function) {
+		$ret = $this->getPostOrElse($key, null);
+		if ($ret !== null) {
+			return $ret;
+		}
+		return $function();
+	}
+
+/**
+ * @throws \Exception
+ */
+	public function getPostOrThrow($key, \Exception $ex) {
+		$ret = $this->getPostOrElse($key, null);
+		if ($ret !== null) {
+			return $ret;
+		}
+		throw $ex;
+	}
+
+	//-----------
+	// Get query
+	//-----------
+
+	public function getQuery($key = null) {
+		return $this->getQueryOrElse($key, null);
+	}
+
+	public function getQueryOrElse($key, $default = null) {
+		if ($key === null) {
+			return $_GET;
+		}
+		$ret = Hash::get($_GET, $key);
+		if ($ret !== null) {
+			return $ret;
+		}
+		return $default;
+	}
+
+	public function getQueryOrCall($key, callable $function) {
+		$ret = $this->getQueryOrElse($key, null);
+		if ($ret !== null) {
+			return $ret;
+		}
+		return $function();
+	}
+
+/**
+ * @throws \Exception
+ */
+	public function getQueryOrThrow($key, \Exception $ex) {
+		$ret = $this->getQueryOrElse($key, null);
+		if ($ret !== null) {
+			return $ret;
+		}
+		throw $ex;
+	}
+
+	//-----------
+	// Input
+	//-----------
+
+	public function getInput($key) {
+		return $this->getInputOrElse($key, null);
+	}
+
+	public function getInputOrElse($key, $default = null) {
+		return $this->getPostOrCall($key, function() use($key, $default) {
+			return $this->getQueryOrElse($key, $default);
+		});
+	}
+
+	public function getInputOrCall($key, callable $function) {
+		return $this->getPostOrCall($key, function() use($key, $function) {
+			return $this->getQueryOrCall($key, function() use($function) {
+				return $function();
+			});
+		});
+	}
+
+/**
+ * @throws \Exception
+ */
+	public function getInputOrThrow($key, \Exception $ex) {
+		return $this->getPostOrCall($key, function() use($key, $ex) {
+			return $this->getQueryOrThrow($key, $ex);
+		});
 	}
 }
--- Controller/MonadController.php
+++ Controller/MonadController.php
@@ -1,7 +1,6 @@
 <?php
 
 App::uses('Controller', 'Controller');
-App::uses('HashMonad', 'Monaca.Lib');
 App::uses('MonadComponent', 'Monaca.Controller/Component');
 App::uses('MonadView', 'Monaca.View');
 
@@ -12,11 +11,7 @@ class MonadController extends Controller {
 	// Properties
 	//
 	//--------------------------------------
-	public $components = array('Monad');
-
-	public $requestData;
-
-	public $requestQuery;
+	public $components = array('Monaca.Monad');
 
 
 	//--------------------------------------
@@ -30,13 +25,6 @@ class MonadController extends Controller {
  */
 	public function __construct($request = null, $response = null) {
 		parent::__construct($request, $response);
-
-		$this->requestData = new HashMonad($request->data);
-		$this->requestQuery = new HashMonad($request->query);
-	}
-
-	public function beforeFilter() {
-		parent::beforeFilter();
 
 		$this->viewClass = 'Monaca.Monad';
 	}
